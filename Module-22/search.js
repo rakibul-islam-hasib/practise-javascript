64aa7ca8750414b8679e4f22da262f007b003fd4
Hasib Hossain <boraborhasib@gmail.com>
2023-02-01 22:14:50 +0600

    all ok

---
+++ Module-22/search.js
@@ -0,0 +1,5 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki , bosonto kale tomay bolte pari ni, sada sada kala kala rong jomeche sada kala';
+
+
+var scarchReselt = lyrics.includes('pokkhi');
+console.log(scarchReselt);
