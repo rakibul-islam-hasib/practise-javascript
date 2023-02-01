3dc9b20443e74fa8139eec9fae1b56633216fd86
Hasib Hossain <boraborhasib@gmail.com>
2023-02-01 22:55:45 +0600

    all ok

--- Module-22/search.js
+++ Module-22/search.js
@@ -1,5 +1,8 @@
 const lyrics = 'Tumi bondhu kala pakhi ami jeno ki , bosonto kale tomay bolte pari ni, sada sada kala kala rong jomeche sada kala';
 
 
-var scarchReselt = lyrics.includes('pokkhi');
-console.log(scarchReselt);
+// var scarchReselt = lyrics.includes('pokkhi');
+// console.log(scarchReselt);
+
+// var cika = lyrics.startsWith('Tumi')
+// var 
