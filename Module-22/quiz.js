bb1b0fa7d10a4e60981aee59678101cee7234c10
Hasib Hossain <boraborhasib@gmail.com>
2023-02-02 20:35:30 +0600

    Console

---
+++ Module-22/quiz.js
@@ -0,0 +1,7 @@
+// console.log(Math.round(Math.random()*10));
+// console.log(Math.floor(13.67));
+// console.log(Math.abs(-78));
+function add(a, b){
+    return a + b;
+  }
+  console.log(add("adam" + "eve"))
