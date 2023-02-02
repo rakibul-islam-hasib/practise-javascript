1c7bdce65569dbf34bb89e75a5845f797e20aed5
Hasib Hossain <boraborhasib@gmail.com>
2023-02-02 14:46:16 +0600

    all ok l

---
+++ Module-22/math.js
@@ -0,0 +1,5 @@
+// console.log(Math.random().toFixed(2))
+
+
+const myMath = Math.floor(Math.random()*20)
+console.log(myMath);
--- Module-22/split-slice.js
+++ Module-22/split-slice.js
@@ -1,3 +1,5 @@
 const lyrics = 'Tumi bondhu kala pakhi ami jeno ki , bosonto kale tomay bolte pari ni, sada sada kala kala rong jomeche sada kala';
 
-console.log(lyrics.split(''));
+// console.log(lyrics.split(''));
+let out = lyrics.slice(8,20);
+console.log(out);
