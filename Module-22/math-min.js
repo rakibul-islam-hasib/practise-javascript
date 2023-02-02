c88decaab6b9dcc432875035018398bc992540c0
Hasib Hossain <boraborhasib@gmail.com>
2023-02-02 15:11:28 +0600

    all ok l

--- Module-22/math-min.js
+++ Module-22/math-min.js
@@ -2,8 +2,16 @@
 
 // console.log(Math.min(yahoo));
 
-function minmum(mith) {
-    
-}
+// function minmum(mith) {
+//     // console.log(mith);
+//     for (let i = 0; i < mith.length; i++) {
+//         const element = array[i];
+//         console.log(Math.min(element));
+        
+//     }
+// }
 
 let yahoo = [12,85,54,96,45,75,41,465];
+// minmum(yahoo);
+
+console.log(Math.min(...yahoo));
